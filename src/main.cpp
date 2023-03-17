4ccfe4fb81d13a4ff4eb29a23acffbf0d97eaa22
gautch29 <gauthier.baron@icloud.com>
2023-03-17 17:26:24 +0100

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include "main.hpp"
 #include "BLC/battery.hpp"
-
+extern "C" {
+#include <pigpio.h>
+}
 
 int main(int argc, char* argv[]){
     char* deviceName = "dev/tty/...";
