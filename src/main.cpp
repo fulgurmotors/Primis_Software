c56509420b39c5560316f1a75b9ce51c218ad8fa
gautch29 <gauthier.baron@icloud.com>
2023-03-20 16:55:34 +0100

    qdfq

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,13 @@ int main(int argc, char* argv[]){
     //char deviceName[] = "dev/usb/hiddev0";
 
     //int a = batteryPercentage(deviceName);
-    int angle = 0;
-    int monte = 1;
 
     while(true){
-        if(monte) angle++;
-        else angle--;
-        if(angle == 255) monte = 0;
-        if(angle == 0) monte = 1;    
-        gpioPWM(12,angle);
-        sleep_for(milliseconds(50));
+        gpioPWM(12,0);
+        sleep_for(milliseconds(10000));
+        
+        gpioPWM(12,255);
+        sleep_for(milliseconds(10000));
     }
     return 0;
 }
