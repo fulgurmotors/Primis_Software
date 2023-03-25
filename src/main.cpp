27fa521cc675e0ad59e074a1947a3e3aa53800f0
gautch29 <82055601+gautch29@users.noreply.github.com>
2023-03-25 12:21:37 +0100

    hgf

--- src/main.cpp
+++ src/main.cpp
@@ -51,9 +51,9 @@ int main(int argc, char* argv[]){
 
 float raspberryPiCPUTemperature(){
     string fileName = "/sys/class/thermal/thermal_zone0/temp";
-    ifstream piCpuTempFile;
+    std::ifstream piCpuTempFile;
     float piCpuTemp = 0.0;
-    stringstream buffer;
+    std::stringstream buffer;
     piCpuTempFile.open(fileName);
     buffer << piCpuTempFile.rdbuf();
     piCpuTempFile.close();
