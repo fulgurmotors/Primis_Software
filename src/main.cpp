b4f90e135f4da9c6de95c51f1be276375e5cf51a
gautch29 <gauthier.baron@icloud.com>
2023-04-16 15:43:53 +0200

    qsdf

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,10 @@ int main(int argc, char* argv[]){
     setTurnSignalLight(BLC_Handle,3);
     sleep_for(milliseconds(1000));
     setTurnSignalLight(BLC_Handle,0);
+    sleep_for(milliseconds(1000));
+    setHeadLight(BLC_Handle, 0);
+
+
 
     serClose(BLC_Handle);
 
